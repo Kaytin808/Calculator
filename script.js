10d477d1fd061b098297932971a3fc4e3212871c
Justin Moore <kaytin143@gmail.com>
2021-09-22 22:46:02 -0700

    Fixed claculator positioning and operation function

--- script.js
+++ script.js
@@ -12,92 +12,60 @@ function multiply(a,b) {
 function divide(a,b) {
     return a / b;
 }
+// Operate Function //
 function operator (a, b, c) {
     let output = 0;
   
-    try {
-  
-      switch(c) {
-        case '+':
-          output = a + b
-          break;
-  
-        case 'x':
-          output = a * b
-          break;
-  
-        case '-':
-          output = a - b
-          break;
-  
-       case '/': 
-          if (b === 0) {
-            throw "Can't divide by 0!"
-          } else {
-            output = a / b
-          }
-          break;
+    if (c === '+') {
+      output = a + b;
+      return output;
+     } else if (c === '-') {
+        output = a - b;
+        return output;
+      } else if (c ==='/') {
+        output = a / b;
+        return output;
+      } else {
+        output = a * b;
+        return output;
       }
     }
-    catch(e) {
-      console.log("There's an error: ", e)
-    }
-  
-    return output;
+let firstInput = '';
+let secondInput = '';
+let currentOp = '';
+const results_div = document.querySelector('.screen')
+const numberBtns = document.querySelectorAll('.numbers')
+numberBtns.forEach((numbers) => {
+numbers.addEventListener('click', () => {
+  if (currentOp === '') {
+    firstInput += numbers.value;
+    results_div.innerHTML= firstInput;
+  } else {
+    secondInput += numbers.value;
+    results_div.innerHTML= firstInput + currentOp + secondInput
   }
-// Empty variables for saving number inputs //
-  const results_div = document.querySelector('.screen')
-  var firstNum = "";
-  var secondNum = "";
-var storedNum ="";
- var currentOp = "";
-const numbers = document.querySelectorAll(".numbers")
-// add a click listener to every butttons on the calculator //
-numbers.forEach((numbers) => {
-  numbers.addEventListener('click', function (){
-    // When clicked, stored into first number and saved into stored number //
-    firstNum += numbers.value;
-    // Whatever you clicked will show up on the calculator screen //
-    results_div.innerHTML = firstNum;
-    // to test if its working
+});
+});
+ // listeners for operators //
+const operatorBtns = document.querySelectorAll('.operator');
+// operator buttons //
+operatorBtns.forEach((operators) => {
+  operators.addEventListener('click', () => {
+currentOp += operators.value;
+results_div.innerHTML= firstInput + currentOp
   })
 })
-// making a button listener for operators
-const buttons = document.querySelectorAll('.operator');
+// equals //
+const equals = document.querySelector('.equals')
+equals.addEventListener('click', () => {
+  var results = operator(parseInt(firstInput),parseInt(secondInput),(currentOp))
+  results_div.innerHTML= results;
+})
+// Clear button //
 const clearBtn = document.querySelector('.c');
-
-clearBtn.addEventListener('click', function(){
-  storedNum = "";
-  firstNum ="";
-  secondNum = "";
-  currentOp ="";
-  results_div.innerHTML=0;
- })
-buttons.forEach((buttons) => {
-  buttons.addEventListener('click', function() {
-   // when number is clicked, saves that first number //
-    storedNum = firstNum;
-    // saves current Operator //
-    currentOp += buttons.value;
-    // Saves a Second number and stores it for later calculation //
-    numbers.forEach((numbers) => {
-      numbers.addEventListener('click', function(){
-        secondNum += numbers.value;
-        results_div.innerHTML = secondNum;
-        // Clear button //
-        
-      })
-    })
-  })
+clearBtn.addEventListener('click', () => {
+  results_div.innerHTML = 0;
+  firstInput = '';
+  secondInput ='';
+  currentOp ='';
 })
-// invokes the operator function when equals button is clicked //
-const equals = document.querySelector('.equals');
-equals.addEventListener('click', function(){
-var results = operator(parseFloat(storedNum), parseFloat(secondNum),(currentOp))
-  results_div.innerHTML = results;
-})
-
-// Make Clear button,
-// When Clear button is pressed, should reset everything back
-// Make event listener click when clear button pressed
-// Once it hears the click, make a function that resets everything back to 0
